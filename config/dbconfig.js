c83e7a1f49cecbfa7871ddd8947ba2b55b904cb2
Devil-9 <johndoe@example.com>
2024-06-08 01:32:17 +0530

    init 1

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,4 +1,5 @@
 module.exports = {
     secret: 'yoursecret',
-    database: 'mongodb+srv://Devil03:Nik@309@rentmech.hyhqp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    // database: 'mongodb+srv://Auther:Rent2369@cluster0.7yqgmcq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    database: 'mongodb://Auther:Rent2369@ac-ktkaejn-shard-00-00.7yqgmcq.mongodb.net:27017,ac-ktkaejn-shard-00-01.7yqgmcq.mongodb.net:27017,ac-ktkaejn-shard-00-02.7yqgmcq.mongodb.net:27017/?ssl=true&replicaSet=atlas-c70ch9-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0'
 }
