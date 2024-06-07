46d58c642038dc3de4340ff0f6f21d0d505bc921
Devil-9 <johndoe@example.com>
2024-06-08 01:32:53 +0530

    init 2

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,5 +1,5 @@
 module.exports = {
     secret: 'yoursecret',
-    // database: 'mongodb+srv://Auther:Rent2369@cluster0.7yqgmcq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-    database: 'mongodb://Auther:Rent2369@ac-ktkaejn-shard-00-00.7yqgmcq.mongodb.net:27017,ac-ktkaejn-shard-00-01.7yqgmcq.mongodb.net:27017,ac-ktkaejn-shard-00-02.7yqgmcq.mongodb.net:27017/?ssl=true&replicaSet=atlas-c70ch9-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0'
+    database: 'mongodb+srv://Auther:Rent2369@cluster0.7yqgmcq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    // database: 'mongodb://Auther:Rent2369@ac-ktkaejn-shard-00-00.7yqgmcq.mongodb.net:27017,ac-ktkaejn-shard-00-01.7yqgmcq.mongodb.net:27017,ac-ktkaejn-shard-00-02.7yqgmcq.mongodb.net:27017/?ssl=true&replicaSet=atlas-c70ch9-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0'
 }
